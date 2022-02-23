be8f9b0e667297e6eecd0bffba80a18490174fdd
nitkis <nitin.123048@gmail.com>
2022-02-23 15:11:33 +0000

    first commit, added my first maven project to GITHUB

---
+++ src/main/java/com/nitinK/app/App.java
@@ -0,0 +1,13 @@
+package com.nitinK.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I have entered into the world of Devops ,Hello DevOps!" );
+    }
+}
